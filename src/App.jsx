39a7673adc5d1e444b21558cf57b8dac3cc98a77
mikaelarosee <mikaelarbaker@gmail.com>
2026-09-23 14:15:41 -0700

    completed component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import Account from "./components/Account.jsx";
+import Books from "./components/Books.jsx";
+import Login from "./components/Login.jsx";
+import Navigation from "./components/Navigation.jsx";
+import Register from "./components/Register.jsx";
+import SingleBook from "./components/SingleBook.jsx";
+import { Routes, Route } from "react-router-dom";
 export default function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/account" element={<Account />} />
+    </Routes>
+  );
 }
---
+++ src/components/SingleBook.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SingleBook() {
+  return <div>SingleBook</div>;
+}
+
+export default SingleBook;
